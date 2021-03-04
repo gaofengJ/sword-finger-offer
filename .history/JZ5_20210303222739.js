60f95aa80afd955c749bb8a26beb587acda418e5
gaofeng_jin <gaofeng_jin@intsig.net>
2021-03-04 15:31:15 +0800

    '变更gitignore'

--- .history/JZ5_20210303222739.js
+++
@@ -1,9 +0,0 @@
-let arr = []
-
-function push (node) {
-  arr.push(node)
-}
-
-function pop () {
-  return arr.shift()
-}
--- .history/JZ9_20210303232533.js
+++
@@ -1,7 +0,0 @@
-/**
- * 时间复杂度：O(1)
- * 空间复杂度：O(1)
- */
-function jumpFloorII (num) {
-  return Math.pow(2, num - 1)
-}
