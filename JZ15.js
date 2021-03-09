39f2b783639ab95b64450c3b66a5b989f4aeef78
gaofengJ <760613910@qq.com>
2021-03-09 22:50:00 +0800

    JZ15

--- JZ15.js
+++ JZ15.js
@@ -1,3 +1,15 @@
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 function ReverseList (pHead) {
-  // write code here
+  let pre = null
+  let next = null
+  while (pHead) {
+    next = pHead.next
+    pHead.next = pre
+    pre = pHead
+    pHead = next
+  }
+  return pre
 }
